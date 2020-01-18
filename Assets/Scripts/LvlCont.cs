8fc0c836c602a19e8001cd002f35d2ff7fd9bde3
UltraCube73 <WhiteBear05@yandex.ru>
2020-01-18 11:54:58 +0400

    -

---
+++ Assets/Scripts/LvlCont.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+[Serializable]
+public class Keys
+{
+    public Sprite keySprite;
+    public KeyCode Key;
+}
+
+public class LvlCont : MonoBehaviour
+{
+    public Keys[] keys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
